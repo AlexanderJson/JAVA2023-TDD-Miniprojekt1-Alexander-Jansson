b9891cb407f7ff62153b4df05ce33e581dc01877
Alexander Jansson <alexanderjson@hotmail.com>
2024-12-17 03:28:30 +0100

    Fixed bug with checkPrime() method, where it passed 1 as a prime number. Also created a findPrimeNumbers in range with testing.

--- src/main/java/org/example/PrimeNumValidator.java
+++ src/main/java/org/example/PrimeNumValidator.java
@@ -7,8 +7,11 @@ public class PrimeNumValidator {
 
     public boolean checkPrime(int num){
         // number needs to be 2 or more than 2 to be a prime number
-        if (num >= 2){
-            // gets the square root of number
+
+        if (num < 2) {
+        return false;
+        }
+            // gets the square root of number,
             int sqrtNum = (int) Math.sqrt(num);
 
             // iterates up to the sqrtNum
@@ -18,10 +21,22 @@ public class PrimeNumValidator {
                     return false;
                 }
             }
-        }
         return true;
     }
 
+    public int findPrimeNumbers(int start, int end){
 
+        int primeNumbers = 0;
+
+
+        for (int i = start; i <= end; i++) {
+            if (checkPrime(i)){
+                primeNumbers++;
+            }
+        }
+        System.out.println("There is a total of: " + primeNumbers + " between "
+        + start + " and " + end);
+        return primeNumbers;
+    }
 
 }
--- src/test/java/org/example/PrimeNumValidatorTest.java
+++ src/test/java/org/example/PrimeNumValidatorTest.java
@@ -1,13 +1,22 @@
 package org.example;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class PrimeNumValidatorTest {
 
-    PrimeNumValidator validator = new PrimeNumValidator();
+    private PrimeNumValidator validator;
+
+    @BeforeEach
+    void init(){
+        validator = new PrimeNumValidator();
+    }
 
     @DisplayName("This method checks if the method " +
             "correctly calculates a prime number")
@@ -26,8 +35,35 @@ class PrimeNumValidatorTest {
         assertFalse(result, "Input is a composite number");
     }
 
-    @DisplayName("")
+
+    @DisplayName("This method checks if the method can correctly find prime numbers between 1-10" )
     @Test
-    void findPrimeNumbersTestRange(){}
+    void findPrimeNumbersTestRangeOneToTen(){
+        int start = 1;
+        int end = 10;
+        int expected = 4;
+        int actual = validator.findPrimeNumbers(start,end);
+        assertEquals(expected,actual,"It should be 4 numbers between 1-10");
+    }
+
+    @DisplayName("This method checks if the method can correctly find prime numbers between 1-10" )
+    @Test
+    void findPrimeNumbersTestFullRange(){
+        int start = 1;
+        int end = 1000;
+        int expected = 168;
+        int actual = validator.findPrimeNumbers(start,end);
+        assertEquals(expected,actual,"It should be 168 numbers between 1-1000");
+    }
+
+//    @DisplayName("This method checks if the method can correctly throw exception if numbers are out of range (1-1000)" )
+//    @Test
+//    void findPrimeNumbersTestOutOfRange(){
+//        int start = 1;
+//        int end = 1001;
+//        assertThrows(IllegalArgumentException.class, () -> {
+//            validator.findPrimeNumbers(start,end);
+//        }, "Input is out of range! Only numbers between 1-1000!!!");
+//    }
 
 }
