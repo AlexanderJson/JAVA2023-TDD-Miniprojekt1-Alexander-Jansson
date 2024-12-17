c9413b1f5eee266adcdac683dd1b8f1e63a1b710
Alexander Jansson <alexanderjson@hotmail.com>
2024-12-17 08:45:02 +0100

    switched to using another method of calculating prime (sieve of eratosthenes) and using bitwise operations to: handle both sum and count calculations, and also because it takes up less meory! With correct testing

--- src/main/java/org/example/PrimeNumValidator.java
+++ src/main/java/org/example/PrimeNumValidator.java
@@ -1,45 +1,154 @@
 package org.example;
 
 import java.util.ArrayList;
+import java.util.BitSet;
+import java.util.List;
 import java.util.Scanner;
 
 public class PrimeNumValidator {
 
-    public boolean checkPrime(int num){
-        // number needs to be 2 or more than 2 to be a prime number
+    private BitSet primeNumbers;
+    private List<Integer> primes;
 
-        if (num < 2) {
-        return false;
-        }
-            // gets the square root of number,
-            int sqrtNum = (int) Math.sqrt(num);
 
-            // iterates up to the sqrtNum
-            for (int i = 2; i <= sqrtNum; i++) {
-                // uses modulus to check if there
-                if (num % i == 0){
-                    return false;
-                }
-            }
-        return true;
-    }
 
-    public int findPrimeNumbers(int start, int end) {
-
+    /**
+     * This method uses Sieve of Eratosthenes to
+     * create a bitset of prime numbers, by removing composite numbers.
+     * It will generate a list of prime numbers from 2 -> n and then reuse the values
+     */
+    public List<Integer> calculatePrime(int n) {
         //Guard clause that checks first so that the input is within range. Else it throws error and interrupts the method completely.
-        if (start < 1 || end > 1000) {
-            throw new IllegalArgumentException("Input must be between 1-1000." +
-                    " Your input was: " + start + " to " + end);
-        }
-            int primeNumbers = 0;
-            for (int i = start; i <= end; i++) {
-                if (checkPrime(i)) {
-                    primeNumbers++;
+        invalidInput(n);
+        //  1 bit is better than boolean 8 bits for the same purpose
+        primeNumbers = new BitSet(n + 1);
+        // All values are initially set to true
+        primeNumbers.set(2, n + 1);
+        /* in outer loop I square [i] first, because all numbers
+        squared are composite numbers(except 1 obviously), this removes all squared composite until N
+         then for each iteration in outer loop, we also take the
+         j = (i*i). This squared numbed of each number, then we just add
+         i until end of range. This removes all composite numbers.
+         Basically if i = 2 then - > 2*2 = 4, 4+2,6+2,8+2 -> 100.
+         But we also set all composite as false and with if statements check
+         every iteration. So it will very quickly be able to iterate no matter range!
+         */
+        for (int i = 2; i * i <= n; i++) {
+            if (primeNumbers.get(i))
+                for (int j = i * i; j <= n; j += i) {
+                    primeNumbers.clear(j);
                 }
+        }
+
+        /* I only convert this to a int list because I needed to return the calculations as integers according to exam specifications.
+            Otherwise I would just return the BitSet as both .cardinality and .stream.sum returns integers.
+            But the core functionality still works, at all the prime numbers are only calculated once during runtime.
+         */
+        List<Integer> primes = new ArrayList<>();
+        for (int i = 2; i <= n; i++){
+            if (primeNumbers.get(i)){
+                primes.add(i);
             }
-            System.out.println("There is a total of: " + primeNumbers + " between "
-                    + start + " and " + end);
-            return primeNumbers;
+        } return primes;
+    }
+
+    public Boolean isPrime(int n) {
+        invalidInput(n);
+        initPrimeList(1000);
+        boolean response =  primeNumbers.get(n);
+        if (response){
+            System.out.println("The number " + n + " is a prime number!");
+            return true;
+        }else {
+            System.out.println("The number " + n + " is not a prime number!");
+        }
+        return false;
+    }
+
+    public int countPrimes(int n) {
+        invalidInput(n);
+        initPrimeList(n);
+        return primeNumbers.cardinality();
+    }
+
+    public int sumPrimes(int n){
+        invalidInput(n);
+        initPrimeList(n);
+        return primeNumbers.stream().sum();
+    }
+
+    public void printSumPrimes(int n){
+        int sum = sumPrimes(n);
+        System.out.println("The sum of all prime numbers to " + n + " is: " + sum);
+    }
+
+    public void printCountPrimes(int n){
+        int sum = countPrimes(n);
+        System.out.println("The count of all prime numbers to " + n + " is: " + sum);
+    }
+
+    // helper classes
+
+    // lazy init of prime list
+    private void initPrimeList(int n){
+        if (primes == null || primeNumbers == null){
+            primes = calculatePrime(n);
         }
     }
+
+    private void invalidInput(int n){
+        if (n < 1 || n > 1000) {
+            throw new IllegalArgumentException("Input must be between 1-1000." +
+                    " Your input was: " + n);
+        }
+    }
+}
+
+
+
+
+// OLD code, incase of failure. TODO: remove
+
+//    public int findPrimeNumbers(int start, int end) {
+//
+//        //Guard clause that checks first so that the input is within range. Else it throws error and interrupts the method completely.
+//        if (start < 1 || end > 1000) {
+//            throw new IllegalArgumentException("Input must be between 1-1000." +
+//                    " Your input was: " + start + " to " + end);
+//        }
+//            int primeNumbers = 0;
+//            for (int i = start; i <= end; i++) {
+//                if (calculatePrime(i)) {
+//                    primeNumbers++;
+//                }
+//            }
+//            System.out.println("There is a total of: " + primeNumbers + " between "
+//                    + start + " and " + end);
+//            return primeNumbers;
+//        }
+//    }
+
+
+
+
+//    public boolean checkPrime(int num){
+//        // number needs to be 2 or more than 2 to be a prime number
+//
+//        if (num < 2) {
+//        return false;
+//        }
+//            // gets the square root of number,
+//            int sqrtNum = (int) Math.sqrt(num);
+//
+//            // iterates up to the sqrtNum
+//            for (int i = 2; i <= sqrtNum; i++) {
+//                // uses modulus to check if there
+//                if (num % i == 0){
+//                    return false;
+//                }
+//            }
+//        return true;
+//    }
+
+
 
--- src/test/java/org/example/PrimeNumValidatorTest.java
+++ src/test/java/org/example/PrimeNumValidatorTest.java
@@ -4,8 +4,9 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.lang.reflect.Array;
-import java.util.ArrayList;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.BitSet;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -18,59 +19,186 @@ class PrimeNumValidatorTest {
         validator = new PrimeNumValidator();
     }
 
+    /**
+     * These tests are for the calculatePrime() method.
+     * They focus on testing different valid and invalid inputs
+     */
 
-    // Tests for checkPrime()
+  @DisplayName("This method tests if calculatePrime can correctly throw error when an invalid range (n) is set")
+  @Test
+  void calculatePrimeTestInvalidRange(){
+        int n = 1001;
+              assertThrows(IllegalArgumentException.class, () -> {
+                  validator.calculatePrime(n);
+        }, "This should throw error if the start is less than 1 or end is more than 1000");
+  }
 
-    @DisplayName("This method checks if the method " +
-            "correctly calculates a prime number")
+
+    @DisplayName("This method checks if isPrime() correctly identifies a prime number as a prime number")
     @Test
-    void checkPrimeTestPrimeNumber(){
-        int input = 29;
-        boolean result = validator.checkPrime(input);
-        assertTrue(result, "Input is a prime number");
+    void isPrimeTestActualPrimeNumber() {
+        int n = 5;
+        boolean actual = validator.isPrime(n);
+        assertTrue(actual);
     }
 
-    @DisplayName("This method checks if the method correctly identfies a non-prime number (composite)")
+    @DisplayName("This method checks if isPrime() correctly identifies a composite number as a composite number")
     @Test
-    void checkPrimeTestCompositeNumber(){
-        int input = 4;
-        boolean result = validator.checkPrime(input);
-        assertFalse(result, "Input is a composite number");
+    void isPrimeTestCompositeNumber() {
+        int n = 4;
+        boolean actual = validator.isPrime(n);
+        assertFalse(actual);
     }
 
-
-    // Tests for findPrimeNumbers()
-
-    @DisplayName("This method checks if the method can correctly find prime numbers between 1-10" )
+    @DisplayName("This method checks if isPrime() correctly identifies a composite number as a composite number")
     @Test
-    void findPrimeNumbersTestRangeOneToTen(){
-        int start = 1;
-        int end = 10;
-        int expected = 4;
-        int actual = validator.findPrimeNumbers(start,end);
-        assertEquals(expected,actual,"It should be 4 numbers between 1-10. The returned sum from method is: " + actual);
-    }
-
-    @DisplayName("This method checks if the method can correctly find prime numbers between 1-10" )
-    @Test
-    void findPrimeNumbersTestFullRange(){
-        int start = 1;
-        int end = 1000;
-        int expected = 168;
-        int actual = validator.findPrimeNumbers(start,end);
-        assertEquals(expected,actual,"It should be 168 numbers between 1-1000. The returned sum from method is: " + actual);
-    }
-
-    @DisplayName("This method checks if the method can correctly throw exception if numbers are out of range (1-1000)" )
-    @Test
-    void findPrimeNumbersTestOutOfRange(){
-        int start = -1;
-        int end = 1001;
+    void isPrimeTestIfInvalidRange() {
+        int n = 1001;
         assertThrows(IllegalArgumentException.class, () -> {
-            validator.findPrimeNumbers(start,end);
+            validator.calculatePrime(n);
         }, "This should throw error if the start is less than 1 or end is more than 1000");
     }
 
+    /**
+     * These tests are for the countPrimes() method.
+     */
+    @DisplayName("This method tests if calculatePrime can correctly identify a prime num")
+    @Test
+    void calculatePrimeTestPrimeNumberRangeToTen() {
+        int n = 10;
+        int expected = 4;
+        int primeNums = validator.countPrimes(n);
+        assertEquals(expected, primeNums,"there should be 4 prime nums between 1-10");
+    }
 
+    @DisplayName("This method tests if calculatePrime can correctly identify a prime num (full range)")
+    @Test
+    void calculatePrimeTestPrimeNumberFullRange() {
+        int n = 1000;
+        int expected = 168;
+        int primeNums = validator.countPrimes(n);
+        assertEquals(expected, primeNums,"there should be 4 prime nums between 1-10");
+    }
+
+    @DisplayName("This method checks if isPrime() correctly identifies a composite number as a composite number")
+    @Test
+    void countPrimeTestIfInvalidRange() {
+        int n = 1001;
+        assertThrows(IllegalArgumentException.class, () -> {
+            validator.calculatePrime(n);
+        }, "This should throw error if the start is less than 1 or end is more than 1000");
+    }
+
+    @DisplayName("This method checks if sumPrimes correctly add all values from a range of 2-10")
+    @Test
+    void sumPrimesRangeToTen() {
+        int n = 10;
+        int actual = validator.sumPrimes(n);
+        int expected = 17;
+        assertEquals(expected,actual,"The total sum of the 4 prime nums in 10 is 17");
+    }
+
+    @DisplayName("This method checks if sumPrimes correctly add all values from a range of 2-100")
+    @Test
+    void sumPrimesRangeToHundred() {
+        int n = 100;
+        int actual = validator.sumPrimes(n);
+        int expected = 1060;
+        assertEquals(expected,actual,"The total sum of the 4 prime nums in 100 is 1060");
+    }
+
+    @DisplayName("This method checks if sumPrimes correctly add all values from a range of 2-1000")
+    @Test
+    void sumPrimesRangeToThousand() {
+        int n = 1000;
+        int actual = validator.sumPrimes(n);
+        int expected = 76127;
+        assertEquals(expected,actual,"The total sum of the 4 prime nums in 1000 is 76127");
+    }
+
+
+    @DisplayName("This method checks if isPrime() correctly identifies a composite number as a composite number")
+    @Test
+    void sumPrimesTestIfInvalidRange() {
+        int n = 1001;
+        assertThrows(IllegalArgumentException.class, () -> {
+            validator.sumPrimes(n);
+        }, "This should throw error if the start is less than 1 or end is more than 1000");
+    }
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//    // Tests for checkPrime()
+//
+//    @DisplayName("This method checks if the method " +
+//            "correctly calculates a prime number")
+//    @Test
+//    void checkPrimeTestPrimeNumber(){
+//        int input = 29;
+//        boolean result = validator.checkPrime(input);
+//        assertTrue(result, "Input is a prime number");
+//    }
+//
+//    @DisplayName("This method checks if the method correctly identfies a non-prime number (composite)")
+//    @Test
+//    void checkPrimeTestCompositeNumber(){
+//        int input = 4;
+//        boolean result = validator.checkPrime(input);
+//        assertFalse(result, "Input is a composite number");
+//    }
+//
+//
+//    // Tests for findPrimeNumbers()
+//
+//    @DisplayName("This method checks if the method can correctly find prime numbers between 1-10" )
+//    @Test
+//    void findPrimeNumbersTestRangeOneToTen(){
+//        int start = 1;
+//        int end = 10;
+//        int expected = 4;
+//        int actual = validator.findPrimeNumbers(start,end);
+//        assertEquals(expected,actual,"It should be 4 numbers between 1-10. The returned sum from method is: " + actual);
+//    }
+//
+//    @DisplayName("This method checks if the method can correctly find prime numbers between 1-10" )
+//    @Test
+//    void findPrimeNumbersTestFullRange(){
+//        int start = 1;
+//        int end = 1000;
+//        int expected = 168;
+//        int actual = validator.findPrimeNumbers(start,end);
+//        assertEquals(expected,actual,"It should be 168 numbers between 1-1000. The returned sum from method is: " + actual);
+//    }
+//
+//    @DisplayName("This method checks if the method can correctly throw exception if numbers are out of range (1-1000)" )
+//    @Test
+//    void findPrimeNumbersTestOutOfRange(){
+//        int start = -1;
+//        int end = 1001;
+//        assertThrows(IllegalArgumentException.class, () -> {
+//            validator.findPrimeNumbers(start,end);
+//        }, "This should throw error if the start is less than 1 or end is more than 1000");
+//    }
+//
+//    @Test
+//    void sumPrimeNumsTestAllPrimeNumbers(){
+//
+//    }
